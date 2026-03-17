a87352048a203f0f05fa037e7f867e7f631e108a
Muhammed Sinan <sinankuttasseri123@gmail.com>
2026-03-17 19:49:46 +0530

    lenis

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+  duration: 1.5,
+  smoothWheel: true,
+});
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 createRoot(document.getElementById("root")!).render(<App />);
